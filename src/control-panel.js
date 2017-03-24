76443381950c99f7029ff3ef5a00c0615e5c8a50
Rachel Ralston <rachelralston@gmail.com>
2017-03-23 18:12:27 -0700

    got most of the control panel completed

---
+++ src/control-panel.js
@@ -0,0 +1,84 @@
+import {Dispatcher, Store} from './flux'
+
+const controlPanelDispatcher = new Dispatcher()
+const UPDATE_USERNAME = 'UPDATE_USERNAME'
+const UPDATE_FONT_SIZE = 'UPDATE_FONT_SIZE'
+
+const userNameUpdateAction = name => {
+  return {
+    type: UPDATE_USERNAME,
+    value: name
+  }
+}
+
+const fontSizeUpdateAction = size => {
+  return {
+    type: UPDATE_FONT_SIZE,
+    value: size
+  }
+}
+
+document.getElementById('userNameInput').addEventListener('input', 
+  ({target}) => {
+    const name = target.value
+    console.log("Dispatching...", name)
+    controlPanelDispatcher.dispatch(userNameUpdateAction(name))
+  })
+
+document.forms.fontSizeForm.fontSize.forEach(element => {
+  element.addEventListener('change', ({target}) => {
+    controlPanelDispatcher.dispatch(fontSizeUpdateAction(target.value))
+  })
+})
+
+class UserPrefsStore extends Store {
+
+  getInitialState() {
+    console.log('prefs',localStorage['preferences'])
+    if(localStorage['preferences']) {
+      return JSON.parse(localStorage['preferences']
+    } else {
+      return {
+        userName: "IAmALeafyPlant",
+        fontSize: "small"
+      }
+    }
+    // return localStorage['preferences'] ? JSON.parse(localStorage['preferences'] : {
+    //   userName: "IAmALeafyPlant",
+    //   fontSize: "small"
+    // }
+  }
+  __onDispatch(action) {
+    switch(action.type) {
+      case UPDATE_USERNAME:
+        this.__state.userName = action.value
+        this.__emitChange();
+        break
+
+      case UPDATE_FONT_SIZE:
+        this.__state.fontSize = action.value
+        this.__emitChange();
+        break
+    }
+  }
+  getUserPreferences() {
+    return this.__state
+  }
+}
+
+const userPrefsStore = new UserPrefsStore(controlPanelDispatcher)
+
+userPrefsStore.addListener(state => {
+  console.info('current State:::', state)
+  render(state)
+  localStorage['preferences'] = JSON.stringify(state)
+})
+
+const render = ({userName, fontSize}) => {
+  document.getElementById("userName").innerText = userName
+  document.getElementsByClassName("container")[0].style.fontSize = 
+    fontSize === "small" ? "16px" : "24px"
+  document.forms.fontSizeForm.fontSize.falue = fontSize
+}
+
+render(userPrefsStore.getUserPreferences())
